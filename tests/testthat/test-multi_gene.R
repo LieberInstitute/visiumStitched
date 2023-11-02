ae143a86e771302fc7aa85548ab56cb4d34d6b37
Nick-Eagles <nick.eagles@libd.org>
2023-11-02 11:18:01 -0400

    Add unit tests for 'spot_plot_z_score'

---
+++ tests/testthat/test-multi_gene.R
@@ -0,0 +1,49 @@
+library(SpatialExperiment)
+
+test_that(
+    "spot_plot_z_score",
+    {
+        spe <- fetch_data(type = "spatialDLPFC_Visium_example_subset")
+
+        #   Bad gene names (gene symbols are not in rownames)
+        sample_id <- unique(spe$sample_id)[1]
+        genes = 'MOBP'
+        expect_error(
+            spot_plot_z_score(spe, genes, sample_id),
+            "does not contain the selected genes"
+        )
+        
+        #   Bad sample ID
+        sample_id = 'asdasdasdasd'
+        genes = rownames(spe)[1:10]
+        expect_error(
+            spot_plot_z_score(spe, genes, sample_id),
+            "must exist and contain the ID"
+        )
+
+        #   Bad assay name
+        sample_id <- unique(spe$sample_id)[1]
+        genes = rownames(spe)[1:10]
+        expect_error(
+            spot_plot_z_score(spe, genes, sample_id, assayname = "asfasdad"),
+            "is not an assay"
+        )
+
+        #   Specifying arguments technically accepted by "..." but internally
+        #   handled and nonsensical
+        expect_error(
+            spot_plot_z_score(spe, genes, sample_id, is_discrete = TRUE),
+            "" #TODO
+        )
+        expect_error(
+            spot_plot_z_score(spe, genes, sample_id, var_name = TRUE),
+            "" #TODO
+        )
+
+        #   Here we essentially just check that the function runs without errors
+        expect_equal(
+            class(spot_plot_z_score(spe, genes, sample_id)),
+            c("gg", "ggplot")
+        )
+    }
+)
