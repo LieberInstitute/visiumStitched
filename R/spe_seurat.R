57e54468519a4155e7b144e84e4037d564d04ce9
Nick-Eagles <nick.eagles@libd.org>
2023-11-03 14:32:28 -0400

    Add a function for converting from SpatialExperiment to Seurat. Function body and roxygen, but no unit tests yet

---
+++ R/spe_seurat.R
@@ -0,0 +1,78 @@
+#' Convert a \code{SpatialExperiment} object to a \code{Seurat} object
+#'
+#' Given a \code{SpatialExperiment} object, first \code{as.Seurat()} is run,
+#' which operates on \code{SingleCellExperiment} objects. The remaining
+#' components (images, spatial coordinates) are added manually. The actual
+#' appearance of images are buggy for now.
+#'
+#' @param spe A \code{SpatialExperiment} with colData columns \code{in_tissue},
+#' \code{array_row_transformed}, \code{array_col_transformed},
+#' \code{pxl_row_in_fullres_transformed}, and \code{pxl_col_in_fullres_transformed}
+#' @param verbose A logical(1) vector. If true, print status updates about the
+#' conversion process
+#'
+#' @return A \code{Seurat} object
+#'
+#' @export
+#' @author Nicholas J. Eagles
+#' @import Seurat spatialLIBD SpatialExperiment SummarizedExperiment
+#'
+#' @examples
+#'
+#' #   Grab an example SpatialExperiment and add dummy "transformed" versions of
+#' #   spatial coordinates, which are to be converted
+#' spe <- spatialLIBD::fetch_data(type = "spatialDLPFC_Visium_example_subset")
+#' for (col_name in c('array_row', 'array_col', 'pxl_row_in_fullres', 'pxl_col_in_fullres')) {
+#'     spe[[paste0(colname, '_transformed')]] = spe[[col_name]]
+#' }
+#'
+#' #   Plot age spatially for the first sample
+#' seur = spe_to_seurat(spe)
+spe_to_seurat = function(spe, verbose = TRUE) {
+    SPOT_DIAMETER = 55e-6
+
+    if (verbose) message("Running 'as.Seurat(spe)'...")
+    seur = as.Seurat(spe)
+
+    for (sample_id in unique(spe$sample_id)) {
+        if (verbose) {
+            message(
+                sprintf(
+                    "Adding spot coordinates and images for sample %s...",
+                    sample_id
+                )
+            )
+        }
+        spe_small = spe[, spe$sample_id == sample_id]
+
+        coords = colData(spe_small)[
+            ,
+            c(
+                'in_tissue', 'array_row_transformed', 'array_col_transformed',
+                'pxl_row_in_fullres_transformed', 'pxl_col_in_fullres_transformed'
+            )
+        ]
+        colnames(coords) = c('tissue', 'row', 'col', 'imagerow', 'imagecol')
+        coords$tissue = as.integer(coords$tissue)
+        coords = as.data.frame(coords)
+
+        this_img = array(
+            t(col2rgb(imgRaster(spe_small))),
+            dim = c(dim(imgRaster(spe_small)), 3)
+        ) / 256
+
+        seur@images[[sample_id]] = Seurat:::VisiumV1(
+            image = this_img,
+            scale.factors = scalefactors(
+                spot = NA, fiducial = NA, hires = NA, lowres = scaleFactors(spe_small)
+            ),
+            coordinates = coords,
+            spot.radius = SPOT_DIAMETER / scaleFactors(spe_small),
+            assay = "originalexp",
+            key = paste0(sample_id, '_')
+        )
+    }
+
+    if (verbose) message("Returning converted object...")
+    return(seur)
+}
