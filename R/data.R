106bee08142b8eaaa7ea538e065f7dacc166d01d
Nick-Eagles <nick.eagles@libd.org>
2024-06-06 12:46:11 -0400

    Add a small SpatialExperiment to use as example data

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' Small \code{SpatialExperiment} of stitched capture areas
+#' 
+#' A \code{SpatialExperiment} with 4 rows and 9548 columns consisting of two
+#' stitched-together capture areas in the human lateral septum
+"spe_small"
