05068d514ca6d98ab1109866d820618e12f28915
Nick-Eagles <nickeagles77@gmail.com>
2024-07-18 10:50:05 -0400

    Add a test for prep_imagej_image()

---
+++ tests/testthat/test-prep_imagej_image.R
@@ -0,0 +1,44 @@
+test_that(
+    "prep_imagej_image",
+    {
+        ########################################################################
+        #   Prepare sample_info
+        ########################################################################
+
+        sample_info = tibble(
+            group = "Br2719",
+            capture_area = c("V13B23-283_A1", "V13B23-283_C1", "V13B23-283_D1")
+        )
+        #   Add 'spaceranger_dir' column
+        sr_dir = tempdir()
+        temp = unzip(fetch_data("Visium_LS_spaceranger"), exdir = sr_dir)
+        sample_info$spaceranger_dir = file.path(
+            sr_dir, sample_info$capture_area, 'outs', 'spatial'
+        )
+
+        #   Add ImageJ-output-related columns
+        imagej_dir = tempdir()
+        temp = unzip(fetch_data("Visium_LS_ImageJ_out"), exdir = imagej_dir)
+        sample_info$imagej_xml_path = temp[grep('xml$', temp)]
+        sample_info$imagej_image_path = temp[grep('png$', temp)]
+
+        sample_info = rescale_imagej_inputs(sample_info, out_dir = tempdir())
+
+        ########################################################################
+        #   Tests
+        ########################################################################
+
+        spe_input_dir = tempdir()
+        prep_imagej_image(
+            sample_info, out_dir = spe_input_dir, lowres_max_size = 900
+        )
+
+        #   The expected output file should be produced
+        out_file = file.path(spe_input_dir, "Br2719", 'tissue_lowres_image.png')
+        expect_equal(file.exists(out_file), TRUE)
+
+        #   The image should have the correct maximal dimension size
+        this_image <- imager::load.image(out_file)
+        expect_equal(max(dim(this_image)), 900)
+    }
+)
