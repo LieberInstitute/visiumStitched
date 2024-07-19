86940f5a8fb9f6f059a97f2b940ae4bebd850ced
lcolladotor <lcolladotor@gmail.com>
2024-07-19 16:18:54 -0400

    Delete spe_small which is no longer being used
    Co-authored-by: Nick Eagles <nickeagles77@gmail.com>

--- R/data.R
+++
@@ -1,5 +0,0 @@
-#' Small \code{SpatialExperiment} of stitched capture areas
-#' 
-#' A \code{SpatialExperiment} with 4 rows and 9548 columns consisting of two
-#' stitched-together capture areas in the human lateral septum
-"spe_small"
