68221dd0d0870004abcc4533924cb872559c7750
Nick-Eagles <nick.eagles@libd.org>
2024-08-23 13:00:21 -0400

    Use temporary files to simulate persistence of objects across examples and tests

--- tests/testthat/test-prep_fiji_coords.R
+++ tests/testthat/test-prep_fiji_coords.R
@@ -5,25 +5,36 @@ test_that(
         #   Prepare sample_info
         ########################################################################
 
-        if (!(exists("sample_info"))) {
-            sample_info <- tibble(
+        if (file.exists("sample_info.rds")) {
+            sample_info <- readRDS('sample_info.rds')
+        } else {
+            sample_info <- dplyr::tibble(
                 group = "Br2719",
                 capture_area = c("V13B23-283_A1", "V13B23-283_C1", "V13B23-283_D1")
             )
             #   Add 'spaceranger_dir' column
             sr_dir <- tempdir()
-            temp <- unzip(fetch_data("visiumStitched_brain_spaceranger"), exdir = sr_dir)
+            temp <- unzip(
+                spatialLIBD::fetch_data("visiumStitched_brain_spaceranger"),
+                exdir = sr_dir
+            )
             sample_info$spaceranger_dir <- file.path(
                 sr_dir, sample_info$capture_area, "outs", "spatial"
             )
 
             #   Add Fiji-output-related columns
             fiji_dir <- tempdir()
-            temp <- unzip(fetch_data("visiumStitched_brain_Fiji_out"), exdir = fiji_dir)
+            temp <- unzip(
+                spatialLIBD::fetch_data("visiumStitched_brain_Fiji_out"),
+                exdir = fiji_dir
+            )
             sample_info$fiji_xml_path <- temp[grep("xml$", temp)]
             sample_info$fiji_image_path <- temp[grep("png$", temp)]
 
+            ## Re-size images and add more information to the sample_info
             sample_info <- rescale_fiji_inputs(sample_info, out_dir = tempdir())
+
+            saveRDS(sample_info, "sample_info.rds")
         }
 
         ########################################################################
