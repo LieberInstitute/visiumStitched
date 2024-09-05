9721fa5954a9f407a3a31083d8e54503181c06a0
Nick-Eagles <nick.eagles@libd.org>
2024-09-05 14:16:11 -0400

    Add roxygen for functions related to array-coordinate-mapping error metrics

--- R/array_error_metrics.R
+++ R/array_error_metrics.R
@@ -1,3 +1,17 @@
+#' Get keys of neighboring spots
+#'
+#' For a given row of a `tibble()` containing array coordinates, find the
+#' associated spot's neighbors (belonging to the same capture area) and return
+#' their keys.
+#'
+#' @param i An `integer(1)` giving a row index in `coords`.
+#' @param coords A `tibble()` containing `array_row`, `array_col`, `key`, and
+#' `capture_area` columns.
+#'
+#' @return A `character()` of neighboring spot keys.
+#'
+#' @author Nicholas J. Eagles
+#' @keywords internal
 .get_neighbors = function(i, coords) {
     this_array_row = coords[[i, 'array_row']]
     this_array_col = coords[[i, 'array_col']]
@@ -18,6 +32,26 @@
     return(neighbor_keys)
 }
 
+#' Calculate fraction of neighbors retained after mapping to new array
+#' coordinates
+#'
+#' Given `tibble()`s before and after mapping to new array coordinates,
+#' calculate for each spot the fraction of starting neighboring spots that
+#' were retained in the new array-coordinate system. Add this metric and
+#' return.
+#'
+#' @param coords_new A `tibble()` containing `array_row`, `array_col`, `key`,
+#' and `capture_area` columns, representing data after mapping to new array
+#' coordinates.
+#' @param coords A `tibble()` containing `array_row`, `array_col`, `key`,
+#' and `capture_area` columns, representing data before mapping to new array
+#' coordinates.
+#'
+#' @return A `tibble()` copy of `coords_new` with additional `shared_neighbors`
+#' column.
+#'
+#' @author Nicholas J. Eagles
+#' @keywords internal
 .get_shared_neighbors = function(coords_new, coords) {
     coords_new$shared_neighbors = sapply(
         seq_len(nrow(coords)),
@@ -31,6 +65,34 @@
     return(coords_new)
 }
 
+#' Add error metrics related to array-coordinate mapping
+#'
+#' Given `tibble()`s before and after mapping to new array coordinates,
+#' calculate metrics related to the suitability of the mapping.
+#' 
+#' Add column `shared_neighbors`, the fraction of neighbors a spot started
+#' with that are retained after mapping; add column `euclidean_error`, the
+#' number of multiples of the inter-spot distance a spot must move to be
+#' placed in the new array coordinates.
+#'
+#' @param coords_new A `tibble()` containing `array_row`, `array_col`, `key`,
+#' `pxl_col_in_fullres`, `pxl_row_in_fullres`, `pxl_col_in_fullres_rounded`,
+#' and `pxl_row_in_fullres_rounded` columns, representing data after mapping
+#' to new array coordinates for one `group`.
+#' @param coords A `tibble()` containing `array_row`, `array_col`, `key`,
+#' `pxl_col_in_fullres`, `pxl_row_in_fullres`, `pxl_col_in_fullres_rounded`,
+#' `pxl_row_in_fullres_rounded`, and `capture_area` columns, representing data
+#' before mapping to new array coordinates for one `group`.
+#' @param inter_spot_dist_px A `numeric(1)` giving the number of pixels between
+#' spots for the `group`.
+#'
+#' @return A `tibble()` copy of `coords_new` with additional `shared_neighbors`
+#' and `euclidean_error` columns.
+#'
+#' @importFrom stringr str_split_i
+#' 
+#' @author Nicholas J. Eagles
+#' @keywords internal
 .add_error_metrics = function(coords, coords_new, inter_spot_dist_px) {
     coords_new = coords_new |>
         mutate(
