5137c84b66f789e57fc769e5f7681b2c3f067ebb
Nick-Eagles <nick.eagles@libd.org>
2024-09-30 15:05:08 -0400

    Add an example

--- R/merge_overlapping.R
+++ R/merge_overlapping.R
@@ -19,10 +19,47 @@
 #' @importFrom tidyr pivot_longer pivot_wider
 #' @importFrom tibble rownames_to_column
 #' @importFrom Matrix Matrix
+#' @importFrom BiocGenerics cbind
 #' @export
 #' @author Nicholas J. Eagles
 #'
 #' @examples
+#' if (!exists("spe")) {
+#'     spe <- spatialLIBD::fetch_data(type = "visiumStitched_brain_spe")
+#' }
+#' 
+#' #   Group colData by group and array coordinates
+#' grouped_coldata = colData(spe) |>
+#'     as_tibble() |>
+#'     group_by(group, array_row, array_col)
+#' 
+#' #   Find the first 100 keys that overlap other spots and don't, respectively
+#' overlapping_keys = grouped_coldata |>
+#'     filter(n() > 1) |>
+#'     slice_head(n = 2) |>
+#'     ungroup() |>
+#'     slice_head(n = 100) |>
+#'     pull(key)
+#' nonoverlapping_keys = grouped_coldata |>
+#'     filter(n() == 1) |>
+#'     ungroup() |>
+#'     slice_head(n = 100) |>
+#'     pull(key)
+#' 
+#' #   Built a small SPE containing some overlaps and some non-overlapping spots
+#' small_spe = spe[, c(overlapping_keys, nonoverlapping_keys)]
+#' 
+#' #   Merge overlapping spots
+#' small_spe_merged = merge_overlapping(small_spe)
+#' 
+#' #   All array coordinates have just one unique spot after merging
+#' colData(small_spe_merged) |>
+#'     as_tibble() |>
+#'     group_by(group, array_row, array_col) |>
+#'     summarize(n = n()) |>
+#'     pull(n) |>
+#'     table()
+#' 
 merge_overlapping <- function(spe) {
     #   Find keys corresponding to spots that must be merged
     overlapping_keys = colData(spe) |>
@@ -30,6 +67,25 @@ merge_overlapping <- function(spe) {
         group_by(group, array_row, array_col) |>
         filter(n() > 1) |>
         pull(key)
+    
+    #   Nothing to merge
+    if(length(overlapping_keys) == 0) {
+        return(spe)
+    }
+
+    #   Check assays and halt or warn
+    if (!('counts' %in% names(assays(spe)))) {
+        stop("'counts' assay missing; unable to merge overlapping spots.")
+    }
+    if (length(assays(spe)) > 1) {
+        warning("Dropping assays other than 'counts' for merging.")
+    }
+    assays(spe) = list(counts = assays(spe)$counts)
+
+    if(length(reducedDims(spe)) > 0) {
+        warning("Dropped reducedDims(spe) for merging")
+    }
+    reducedDims(spe) = list()
     
     merged_data =
         #   For the spots to merge, form a tibble with spots as rows and genes
@@ -71,7 +127,7 @@ merge_overlapping <- function(spe) {
     colnames(spe_overlap) = spe_overlap$key
 
     #   Combined the merged spots and non-overlapping spots
-    spe = cbind(spe_overlap, spe[, !(spe$key %in% overlapping_keys)])
+    spe = BiocGenerics::cbind(spe_overlap, spe[, !(spe$key %in% overlapping_keys)])
     
     return(spe)
 }
