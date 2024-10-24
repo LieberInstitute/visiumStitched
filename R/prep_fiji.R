c117e6c3cb80012f079a20b1c39c869e3032e105
Nick-Eagles <nick.eagles@libd.org>
2024-10-24 15:42:56 -0400

    Don't save a local temporary file to cache results for the examples and tests (as this is considered against best practices)

--- R/prep_fiji.R
+++ R/prep_fiji.R
@@ -39,38 +39,31 @@
 #' @author Nicholas J. Eagles
 #'
 #' @examples
-#' sample_info_path = file.path(tempdir(), "sample_info.rds")
-#' if (file.exists(sample_info_path)) {
-#'     sample_info <- readRDS(sample_info_path)
-#' } else {
-#'     sample_info <- dplyr::tibble(
-#'         group = "Br2719",
-#'         capture_area = c("V13B23-283_A1", "V13B23-283_C1", "V13B23-283_D1")
-#'     )
-#'     #   Add 'spaceranger_dir' column
-#'     sr_dir <- tempdir()
-#'     temp <- unzip(
-#'         spatialLIBD::fetch_data("visiumStitched_brain_spaceranger"),
-#'         exdir = sr_dir
-#'     )
-#'     sample_info$spaceranger_dir <- file.path(
-#'         sr_dir, sample_info$capture_area, "outs", "spatial"
-#'     )
-#'
-#'     #   Add Fiji-output-related columns
-#'     fiji_dir <- tempdir()
-#'     temp <- unzip(
-#'         spatialLIBD::fetch_data("visiumStitched_brain_Fiji_out"),
-#'         exdir = fiji_dir
-#'     )
-#'     sample_info$fiji_xml_path <- temp[grep("xml$", temp)]
-#'     sample_info$fiji_image_path <- temp[grep("png$", temp)]
-#'
-#'     ## Re-size images and add more information to the sample_info
-#'     sample_info <- rescale_fiji_inputs(sample_info, out_dir = tempdir())
-#'
-#'     saveRDS(sample_info, sample_info_path)
-#' }
+#' sample_info <- dplyr::tibble(
+#'     group = "Br2719",
+#'     capture_area = c("V13B23-283_A1", "V13B23-283_C1", "V13B23-283_D1")
+#' )
+#' #   Add 'spaceranger_dir' column
+#' sr_dir <- tempdir()
+#' temp <- unzip(
+#'     spatialLIBD::fetch_data("visiumStitched_brain_spaceranger"),
+#'     exdir = sr_dir
+#' )
+#' sample_info$spaceranger_dir <- file.path(
+#'     sr_dir, sample_info$capture_area, "outs", "spatial"
+#' )
+#' 
+#' #   Add Fiji-output-related columns
+#' fiji_dir <- tempdir()
+#' temp <- unzip(
+#'     spatialLIBD::fetch_data("visiumStitched_brain_Fiji_out"),
+#'     exdir = fiji_dir
+#' )
+#' sample_info$fiji_xml_path <- temp[grep("xml$", temp)]
+#' sample_info$fiji_image_path <- temp[grep("png$", temp)]
+#' 
+#' ## Re-size images and add more information to the sample_info
+#' sample_info <- rescale_fiji_inputs(sample_info, out_dir = tempdir())
 #'
 #' spe_input_dir <- tempdir()
 #' out_paths_image <- prep_fiji_image(
